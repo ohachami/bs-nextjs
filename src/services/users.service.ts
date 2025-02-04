cce331d2902e64dc55d6f6ced5d2440d41c5f1f1
omar <ohachami@gmail.com>
2025-02-04 09:34:11 +0100

    feat: add useUser query

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,6 +1,21 @@
 import api from "@/api";
 import { callAsync } from "@/hooks/useAsync";
+import { User } from "@/types/user";
+import { apiPaths } from "@/utils/apiPaths";
 import { useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
+
+export const useUser = () => {
+  
+  return useQuery({
+    queryKey: ["currentUser"],
+    queryFn: async () => {
+      const response = await callAsync<AxiosResponse<User>>(() => api.get(apiPaths.currentUser()));
+      return response.data;
+    },
+  });
+};
+
 
 export const useUsers = () => {
   return useQuery({
