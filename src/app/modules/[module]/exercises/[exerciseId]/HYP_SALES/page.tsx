270327cc714f8dcf30b2bcee3a35a6d0d74c1786
abdellah <abdellah.sabiri.1995@gmail.com>
2025-02-19 20:25:46 +0100

    SD-169: Manufacturing dashboard

--- src/app/modules/[module]/exercises/[exerciseId]/HYP_SALES/page.tsx
+++ src/app/modules/[module]/exercises/[exerciseId]/HYP_SALES/page.tsx
@@ -2,9 +2,20 @@
 
 import HypWrapper from '@/components/sections/HypWrapper';
 import { SBUS } from '@/utils/constants';
+import ConsolidationCombobox from "@/components/common/ConsolidationCombobox";
+import { useSbus } from "@/services/referential.Service";
 
 function PageHyperManu() {
-  
+    const { data: sbus, isLoading, isError } = useSbus();
+
+    const onSelectHandler = (selectedValue: string) => {
+        console.log(selectedValue);
+    };
+
+    if (isLoading) return <p>Loading Sbus...</p>;
+
+    if (isError || !sbus) return <p>Error Loading Sbus!</p>;
+
   return (
     <HypWrapper
       shouldDisableStep={(user) =>{
@@ -22,8 +33,10 @@ function PageHyperManu() {
           'Les BUs commerciales sont en train sont en train de terminer leur version',
       }}
     >
-
-      
+        <ConsolidationCombobox
+            onSelect={onSelectHandler}
+            sbuId={sbus.find((sbu) => sbu.name === SBUS.MANUFACTURING)?.id}
+        />
     </HypWrapper>
   );
 }
