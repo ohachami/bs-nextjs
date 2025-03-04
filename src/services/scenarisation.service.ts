b05a20729df77512bade0de145cd1b53029365f4
mehdaoui-fatima <mehdaoui.fa@gmail.com>
2025-03-04 21:47:05 +0000

    display scenario params page

---
+++ src/services/scenarisation.service.ts
@@ -0,0 +1,20 @@
+import { callApi } from "@/hooks/useApi"
+import { ScenarioDataIF } from "@/types/senarisation"
+import { apiPaths } from "@/utils/apiPaths"
+import { useQuery } from "@tanstack/react-query"
+
+
+//get the consolidated data of the a scenario
+export const useScenarioParams = (consoDataId: string) => {
+    return useQuery({
+        queryKey: ["scenariosData", consoDataId],
+        queryFn: async () => {
+            return await callApi<ScenarioDataIF>({
+                method: "GET",
+                url: apiPaths.ScenarioParams(consoDataId)
+            })
+        },
+        enabled: !!consoDataId
+    })
+
+}
